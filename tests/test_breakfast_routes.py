5df59622c95fac10b8b1a65ddc0446cfa6e68a71
Jerica Huang <jerica.huang@gmail.com>
2022-11-02 14:48:56 -0700

    test name changes bike->breakfast

--- tests/test_breakfast_routes.py
+++ tests/test_breakfast_routes.py
@@ -1,18 +1,18 @@
-def test_get_all_bikes_with_empty_db_returns_empty_list(client):
+def test_get_all_breakfasts_with_empty_db_returns_empty_list(client):
     response = client.get("/breakfast")
     response_body = response.get_json()
 
     assert response.status_code == 200
     assert response_body == []
     
-def test_get_one_bike_with_empty_db_returns_404(client):
+def test_get_one_breakfast_with_empty_db_returns_404(client):
     response = client.get("/breakfast/1")
     response_body = response.get_json()
 
     assert response.status_code == 404
     assert "msg" in response_body
 
-def test_get_one_breakfast_with_populated_db_returns_bike_json(client, two_breakfasts):
+def test_get_one_breakfast_with_populated_db_returns_breakfast_json(client, two_breakfasts):
     response = client.get("/breakfast/1")
     response_body = response.get_json()
 
