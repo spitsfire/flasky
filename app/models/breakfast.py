75ac6973c68f58730905b4ce4bcbb51dd58d4a40
Jasmine Lopez <jasmine@adadevelopersacademy.org>
2022-11-07 14:04:27 -0800

    Livecode Nov 7

--- app/models/breakfast.py
+++ app/models/breakfast.py
@@ -5,13 +5,16 @@ class Breakfast(db.Model):
     name = db.Column(db.String)
     rating = db.Column(db.Float)
     prep_time = db.Column(db.Integer)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'))
+    menu = db.relationship('Menu', back_populates='breakfast_items')
 
     def to_dict(self):
         return {
             "id":self.id,
             "name":self.name,
             "rating":self.rating,
-            "prep_time":self.prep_time
+            "prep_time":self.prep_time,
+            "menu_id":self.menu_id
         }
 
     @classmethod
@@ -19,5 +22,6 @@ class Breakfast(db.Model):
         return cls(
             name=breakfast_dict["name"],
             rating=breakfast_dict["rating"],
-            prep_time=breakfast_dict["prep_time"]
+            prep_time=breakfast_dict["prep_time"],
+            menu_id=breakfast_dict["menu_id"]
         )
